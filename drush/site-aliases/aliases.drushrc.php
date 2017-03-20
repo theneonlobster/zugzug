9d991e7e38d854885a1dea362b95a180721c34e3
David Platek <theneonlobster@gmail.com>
2017-03-20 13:43:14 -0700

    Updated project.yml, ran blt vm, and ran blt local:setup

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,14 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+// [vagrant_machine_name].local
+$aliases['zugzug.local'] = array(
+  // /var/www/[vagrant_machine_name]/docroot
+  'root' => '/var/www/zugzug/docroot',
+  // vagrant_hostname
+  'uri' => 'http://local.zugzug.com',
+  // vagrant_hostname
+  'remote-host' => 'local.zugzug.com',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+);
